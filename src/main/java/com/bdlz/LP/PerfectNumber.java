d556c005ebebfb59dba94517e0f477d0da70a722
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-02 23:24:31 +0530

    Added: Perfect Number

---
+++ src/main/java/com/bdlz/LP/PerfectNumber.java
@@ -0,0 +1,26 @@
+package com.bdlz.LP;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int number, result = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        number = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                result += i;
+            }
+        }
+        if (number == result) {
+            System.out.println("The given number is perfect number");
+        } else {
+            System.out.println("The given number is not perfect number");
+        }
+
+    }
+
+}
